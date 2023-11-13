384de119dd85eeb152160822f318f2abd7ac305c
clover-forever <yungchunchen0618@gmail.com>
2023-11-13 10:44:23 +0800

    create commentRepository.java

---
+++ src/main/java/oes/moduleName/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package oes.moduleName.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import oes.moduleName.entity.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer>{
+
+}
