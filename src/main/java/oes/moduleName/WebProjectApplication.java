d44cf7cd31b742b2808412753c70eea043cb2003
clover-forever <yungchunchen0618@gmail.com>
2023-11-16 15:56:50 +0800

    yaml file and registry

--- src/main/java/oes/moduleName/WebProjectApplication.java
+++ src/main/java/oes/moduleName/WebProjectApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.cloud.client.discovery.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebProjectApplication {
 
 	public static void main(String[] args) {
